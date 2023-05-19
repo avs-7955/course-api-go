e9c3d027edae1e9e437f0ddda836a727ed8d772c
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-05-19 18:24:55 +0530

    model and task division

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+// Model for course - file
+type Course struct {
+	CourseID    string  `json:"cid"`
+	CourseName  string  `json:"cname"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"authordetails"`
 }
+
+type Author struct {
+	FullName string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fake database
+var coursesDb []Course
+
+// helper function - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseName == ""
+}
+
+func main() {
+	fmt.Println("API - Udemy.com")
+}
+
+//TODO: User can get all the courses
+//TODO: Create, delete and update new courses
+//TODO: Helper function to prevent display of courses with no title.
+// Database to be used -> slice
